0d64042755b75a4843cdddca5ff55a2625edeec9
rabby <20golamrabby19@gmail.com>
2025-01-05 16:38:34 +0600

    4th

--- src/main/java/bd/edu/seu/finalproject/model/Student.java
+++ src/main/java/bd/edu/seu/finalproject/model/Student.java
@@ -1,10 +1,8 @@
 package bd.edu.seu.finalproject.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
-@Data
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,4 +13,38 @@ public class Student {
 
     @Column(nullable = false)
     private String password;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // Override toString for better readability
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
